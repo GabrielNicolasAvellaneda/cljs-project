4b5f10ed714d3482bee953caf5c2d2dd075b6d97
gavellaneda <avellaneda.gabriel@gmail.com>
2015-02-02 12:53:32 -0200

    Added support for cljsbuild plugin

--- project.clj
+++ project.clj
@@ -5,5 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2755"]]
+  :source-paths ["src/clj"]
   :plugins [[lein-cljsbuild "1.0.4"]]
-  :cljsbuild {:builds []})
+  :cljsbuild {
+              :builds [{
+                :source-paths ["src/cljs"]
+                :compiler {
+                 :optimizations :whitespace
+                 :output-to "resources/public/project.js"
+                 :pretty-print true}}]})
---
+++ src/cljs/cljs_project/hello.cljs
@@ -0,0 +1,2 @@
+(ns cljs-project.hello)
+(js/alert "Hello, world")
